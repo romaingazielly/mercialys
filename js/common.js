ac1a5a75624f13f5d592ef89f6d71d4651f27f2c
Romain <romain.gazielly@gmail.com>
2013-10-07 18:15:00 +0200

    Ajout bloc agenda et bloc centre commercial

---
+++ js/common.js
@@ -0,0 +1,67 @@
+$(function(){
+	blocCentreCo();
+});
+
+function blocCentreCo() {
+	var curr = 'red';
+
+	$('#map-red').click(function(e){
+		e.preventDefault();
+
+		if(curr != 'red') {
+			$('#map-mercialys-pointer').animate({'background-position-x': '-124px'});
+			$('#map-mercialys-title').fadeOut('fast', function(){
+				$(this).html('Grande surface spécialisée').css({'color':'#b60d1c'}).fadeIn();
+				curr = 'red';
+			});
+		}
+	});
+
+	$('#map-green').click(function(e){
+		e.preventDefault();
+
+		if(curr != 'green') {
+			$('#map-mercialys-pointer').animate({'background-position-x': '-91px'});
+			$('#map-mercialys-title').fadeOut('fast', function(){
+				$(this).html('Centre local de Proximité').css({'color':'#b7d00d'}).fadeIn();
+				curr = 'green';
+			});
+		}
+	});
+
+	$('#map-yellow').click(function(e){
+		e.preventDefault();
+
+		if(curr != 'yellow') {
+			$('#map-mercialys-pointer').animate({'background-position-x': '-57px'});
+			$('#map-mercialys-title').fadeOut('fast', function(){
+				$(this).html('Surface alimentaire').css({'color':'#e7c22c'}).fadeIn();
+				curr = 'yellow';
+			});
+		}
+	});
+
+	$('#map-black').click(function(e){
+		e.preventDefault();
+
+		if(curr != 'black') {
+			$('#map-mercialys-pointer').animate({'background-position-x': '-24px'});
+			$('#map-mercialys-title').fadeOut('fast', function(){
+				$(this).html('Grand centre régional').css({'color':'#262219'}).fadeIn();
+				curr = 'black';
+			});
+		}
+	});
+
+	$('#map-grey').click(function(e){
+		e.preventDefault();
+
+		if(curr != 'grey') {
+			$('#map-mercialys-pointer').animate({'background-position-x': '9px'});
+			$('#map-mercialys-title').fadeOut('fast', function(){
+				$(this).html('Grand centre commercial').css({'color':'#5e5238'}).fadeIn();
+				curr = 'grey';
+			});
+		}
+	});
+}
