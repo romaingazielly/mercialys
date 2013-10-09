c44a86bec5d31259b7859208434a19a32804bb7f
Romain <romain.gazielly@gmail.com>
2013-10-09 18:41:49 +0200

    Ajout direction + debug

--- js/common.js
+++ js/common.js
@@ -9,7 +9,7 @@ function blocCentreCo() {
 		e.preventDefault();
 
 		if(curr != 'red') {
-			$('#map-mercialys-pointer').animate({'background-position-x': '-124px'});
+			$('#map-mercialys-pointer').animate({backgroundPositionX: '-124px'});
 			$('#map-mercialys-title').fadeOut('fast', function(){
 				$(this).html('Grande surface spécialisée').css({'color':'#b60d1c'}).fadeIn();
 				curr = 'red';
@@ -21,7 +21,7 @@ function blocCentreCo() {
 		e.preventDefault();
 
 		if(curr != 'green') {
-			$('#map-mercialys-pointer').animate({'background-position-x': '-91px'});
+			$('#map-mercialys-pointer').animate({backgroundPositionX: '-91px'});
 			$('#map-mercialys-title').fadeOut('fast', function(){
 				$(this).html('Centre local de Proximité').css({'color':'#b7d00d'}).fadeIn();
 				curr = 'green';
@@ -33,7 +33,7 @@ function blocCentreCo() {
 		e.preventDefault();
 
 		if(curr != 'yellow') {
-			$('#map-mercialys-pointer').animate({'background-position-x': '-57px'});
+			$('#map-mercialys-pointer').animate({backgroundPositionX: '-57px'});
 			$('#map-mercialys-title').fadeOut('fast', function(){
 				$(this).html('Surface alimentaire').css({'color':'#e7c22c'}).fadeIn();
 				curr = 'yellow';
@@ -45,7 +45,7 @@ function blocCentreCo() {
 		e.preventDefault();
 
 		if(curr != 'black') {
-			$('#map-mercialys-pointer').animate({'background-position-x': '-24px'});
+			$('#map-mercialys-pointer').animate({backgroundPositionX: '-24px'});
 			$('#map-mercialys-title').fadeOut('fast', function(){
 				$(this).html('Grand centre régional').css({'color':'#262219'}).fadeIn();
 				curr = 'black';
@@ -57,7 +57,7 @@ function blocCentreCo() {
 		e.preventDefault();
 
 		if(curr != 'grey') {
-			$('#map-mercialys-pointer').animate({'background-position-x': '9px'});
+			$('#map-mercialys-pointer').animate({backgroundPositionX: '9px'});
 			$('#map-mercialys-title').fadeOut('fast', function(){
 				$(this).html('Grand centre commercial').css({'color':'#5e5238'}).fadeIn();
 				curr = 'grey';
