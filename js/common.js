3557a1083acd0268639294f01b635b15abd04ff9
Romain <romain.gazielly@gmail.com>
2013-10-16 19:25:43 +0200

    Ajout Patrimoine 90%

--- js/common.js
+++ js/common.js
@@ -1,8 +1,28 @@
 $(function(){
 	blocCentreCo();
 	btnScrollTop();
-});
 
+	// Au clic sur le plan du site 
+	$('#btn-plan').click(function(e){
+		e.preventDefault();
+
+		var $window = $(window),
+		wHeight = $window.height();
+		
+
+		$('.popin-visualizer').css({ 'display': 'block', 'opacity':0 });
+		var imgH = $('.popin-visualizer > div > img').height();
+		$('.popin-visualizer img').css({'margin-top': (wHeight-imgH)/2 });
+		$('.popin-visualizer').animate({ 'opacity': 1 }, 'slow');
+	 });
+
+	// Fait disparaitre la popin
+	$('.popin-visualizer > div > a, .popin-visualizer').click(function(){
+		$('.popin-visualizer').animate({ 'opacity': 0}, 'slow', function(){
+			$('.popin-visualizer').css({ 'display': 'none' });
+		});
+	});
+});
 
 /*-----------bouton retour haut---------*/
 function btnScrollTop(){
